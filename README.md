301a63d2e722616e588db22737a58c087ccf21ee
Aidan Fitzpatrick <aidan@reincubate.com>
2015-11-23 12:08:32 +0000

    First commit of JavaScript Node.js ricloud iCloud API client.

---
+++ README.md
@@ -0,0 +1,146 @@
+# ricloud: iCloud access made easy
+
+This is a sample JavaScript Node.js module for interaction with Reincubate's iCloud API. The Reincubate iCloud API provides powerful programmatic iCloud access to investigators, application developers and integrators. It is RESTful and makes many commonly-accessed forms of data available as JSON feeds.
+
+The API includes functionality for extraction, manipulation and recovery of many types of iOS data, and has functionality to support bulk, scheduled, and realtime data access. It fully supports iOS 9 CloudKit-based iCloud backups, and backups created with the new A9 chipsets.
+
+## JSON feed vs raw file access
+
+There are two core parts to the API: the JSON feed mechanism, and the raw file access mechanism. The JSON feeds come with a number of advantages:
+
+ * Access to feed data is generally faster and scales better than raw file access
+ * App data stored in databases and Plists is prone to change in format and location over time; the JSON feed abstracts away that complexity so that you needn't worry.
+ * Users of the JSON feeds are able to take advantage of Reincubate's proprietary techniques in extracting app data, such that the resultant data is more accurate.
+
+## Installation
+
+The library can be installed with a single command:
+
+```bash
+$ npm install ricloud
+```
+
+### Configuration
+
+The API relies on a set of security credentials, which are stored in an `conf/settings.json` file. This package ships with a default configuration file which enables limited access to the API for demonstration purposes. Full access can be gained by contacting [Reincubate](mailto:enterprise@reincubate.com).
+
+Alternately these security credentials can be specified when calling the APIs (see below).
+
+This file should have the following details:
+
+```json
+{
+  "auth": {
+    "user": "<reincubate user id>",
+    "key": "<reincubate key>"
+  },
+  "icloud": {
+    "apple_id": "<iCloud ID>",
+    "password": "<iCloud password>"
+  }
+}
+```
+
+## Usage
+
+A sample script is included which provides an example of how the API can be used to access a range of datatypes in a way that is compatible with Apple's 2FA mechanism. The sample script can be run like this:
+
+```bash
+$ node ricloud-test.js
+```
+
+### Authentication against the API and listing iCloud data
+
+```javascript
+var riCloud = require('ricloud.js')
+
+api = new riCloud()
+// or to specify user and key instead of using settings.json
+// api = new Reincubate(user, key)
+
+api.login(function(err, data) {
+    console.log(api.devices)
+})
+// or specify Apple ID and password instead of using settings.json with:
+// api.login(appleID, password, callback())
+```
+
+
+That `api.devices` dictionary contains data in this format:
+
+```python
+{'7c7fba66680ef796b916b067077cc246adacf01d': {
+    'colour': '#e4e7e8',
+    'device_name': "Renate's iPhone",
+    'latest-backup': '2015-11-17 16:46:39.000000',
+    'model': 'N71mAP',
+    'name': 'iPhone 6s'},
+ '8e281be6657d4523710d96341b6f86ba89b56df7': {
+    'colour': '#e1e4e3',
+    'device_name': "Renate's iPad",
+    'latest-backup': '2015-11-13 19:35:52.000000',
+    'model': 'J98aAP',
+    'name': 'iPad Pro'},
+}
+```
+
+### Using the JSON feed API
+
+The API is able to return data retrieved from a wide range of apps, and enumerations for some of these are baked into the sample API. However, we have many other types of app feeds available, including Viber, Kik, WeChat, Line, and others.
+
+> We also have functionality such as message undeletion which can be enabled on demand against API keys.
+
+To choose which data types to return in the feed, users can pass a mask of data types to the `requestData` method. To select multiple data types, separate each type with the bitwise OR ``|`` operator. For example to select both SMS and photo data:
+
+```javascript
+# SMS and photo retrieval
+var requested_data = api.data.SMS | api.data.PHOTOS
+```
+
+If no selection is made, the API will return all available data available. The following is an example of how to select which data to retrieve.
+
+```javascript
+var requested_data = api.data.SMS
+var since = new Date(2015, 0, 1)
+api.requestData(device, requested_data, since, function(err,data) {
+    console.log(data)
+})
+```
+
+## Troubleshooting
+
+### The JSON feed returns a message: "Contact enterprise@reincubate.com for access to this data"
+
+This message will be returned when the demonstration key is used. Please contact us for a trial key with access to more data. If you already have a trial key, are you correctly specifying it in your `~/.ricloud.ini` file? Note that the file has a period at the start.
+
+### I'm trying to pull an app's database file by `file_id` but I'm not getting any data back
+
+`file_id`s are derived from an SHA-1 hash of the file's path and name, so they are constant for any given file. If the file's attributes or content change, it won't affect the hash.
+
+However, sometimes app authors change the name of the file they store data in (and sometimes Apple do in new iOS releases). That's why, for instance, there several different `file_id`s to examine when getting WhatsApp data. These `file_id`s could be changed any time an app is updated.
+
+This is one of the reasons we recommend users pull our JSON feeds instead of working with files and manipulating them directly. Using the JSON feeds, one needn't worry over the efficacy of SQL, PList parsing or undeletion, and the JSON feeds are quicker and much simpler to work with.
+
+## Need more functionality?
+
+Reincubate builds world class iOS and app data access and recovery technology. The company was founded in 2008 and was first to market with iOS backup extraction technology, consumer backup decryption, and more recently with enterprise iCloud support. Clients include law enforcement, government and security organisations in the US and internationally, and to corporations as large as Microsoft and IBM.
+
+> Users with simpler needs may wish to try the [iPhone Backup Extractor](<http://www.iphonebackupextractor.com), which provides a set of iCloud functionality better suited to consumers.
+
+With six years' experience helping police forces, law firms and forensics labs access iOS data, the company can help enterprise users with:
+
+* iCloud access and data recovery
+* Recovery of data deleted from SQLite databases
+* Bulk iOS data recovery
+* Forensic examination of iOS data
+* Passcode, password and keybag analysis
+* Custom iOS app data extraction
+* Advanced PList, TypedStream and Mbdb manipulation
+
+Contact [Reincubate](mailto:enterprise@reincubate.com) for more information, or see our site at [reincubate.com](https://www.reincubate.com).
+
+## Terms & license
+
+Users must not use the API in any way that is unlawful, illegal, fraudulent or harmful; or in connection with any unlawful, illegal, fraudulent or harmful purpose or activity. See the `LICENSE` file. Full terms are available from [Reincubate](mailto:enterprise@reincubate.com).
+
+Copyright &copy; Reincubate Ltd, 2011 - 2015, all rights reserved.
---
+++ dist/ricloud.js
@@ -0,0 +1,317 @@
+'use strict'
+var request = require('request')
+var moment = require('moment')
+var fs = require('fs');
+
+// endpoints
+var ENDPOINTS = {
+    'login': "/c/sign-in/",
+    'challenge_2fa': "/c/perform-2fa-challenge/",
+    'submit_2fa': "/c/submit-2fa-challenge/",
+    'download_data': "/c/download-data/",
+    'download_file': "/c/download-file/"
+}
+
+var HOST = 'https://api.icloudextractor.com'
+var API_VER = 1
+
+
+/**
+ * Callback format used by all async functions
+ *
+ * @callback callback
+ * @param {number} err - The error (from the error enum)
+ * @param {*} data
+ */
+
+/**
+ * Primary object for dealing with the API. Used to initialize our client API instance.
+ * @param [user] {string|null} riCloud User ID to authenticate against
+ * @param [key] {string|null} Corresponding authentication key supplied by Reincubate
+ * @constructor
+ */
+var riCloud = function (user, key) {
+    // Attempt to get settings from file
+    var settings = JSON.parse(fs.readFileSync('conf/settings.json', 'utf8'));
+
+    user = user ? user : settings.auth.user
+    key = key ? key : settings.auth.key
+    HOST = settings.endpoints.host ? settings.endpoints.host : HOST
+    API_VER = settings.api_version ? settings.api_version : API_VER
+
+    this.auth = {
+        user: user,
+        pass: key
+    }
+
+    this.data = {
+        SMS: 1,
+        PHOTOS: 2,
+        BROWSER_HISTORY: 4,
+        CALL_HISTORY: 8,
+        CONTACTS: 16,
+        INSTALLED_APPS: 32,
+        WEB_CONTACTS: 64,
+        WEB_LOCATION: 256,
+        WHATSAPP_MESSAGES: 512,
+        SKYPE_MESSAGES: 1024
+    }
+
+    this.error = {
+        GENERAL: 1,
+        TWOFA_REQUIRED: 2
+    }
+
+    this.appleID = settings.icloud.apple_id ? settings.icloud.apple_id : null
+    this.password = settings.icloud.password ? settings.icloud.password : null
+};
+
+
+/**
+ * Log into iCloud
+ * @param [appleID] {string|null} User's Apple ID
+ * @param [password] {string|null} User's Apple password
+ * @param cb {callback} Callback
+ */
+riCloud.prototype.login = function (appleID, password, cb) {
+    var newArgs = argsWithFn(arguments, ['appleID', 'password', 'cb']);
+    appleID = newArgs.appleID
+    password = newArgs.password
+    cb = newArgs.cb
+
+    appleID = appleID ? appleID : this.appleID
+    password = password ? password : this.password
+
+    var data = {
+        email: appleID,
+        password: password
+    }
+
+    if (this.sessionKey) {
+        data.key = this.sessionKey
+    }
+
+    var options = generateOptions(this, 'login', data)
+
+    function callback(error, response, body) {
+        var data
+        if (!error && response.statusCode == 409) {
+            data = JSON.parse(body)
+            if (data.error == '2fa-required') {
+                context.sessionKey = data.data.key
+                context.trustedDevices = data.data.trustedDevices
+                cb(context.error.TWOFA_REQUIRED, null)
+            }
+        } else if (!error && response.statusCode == 200) {
+            data = JSON.parse(body)
+            context.sessionKey = data.key
+            context.devices = data.devices
+            cb(0, null)
+        } else {
+            cb(context.error.GENERAL, response)
+        }
+    }
+
+    var context = this;
+    request(options, callback)
+}
+
+
+/**
+ * Pull data from iCloud from a given point in time.
+ * @param device {string}  Device ID to pull data for
+ * @param [requestedData] {number|null} Bit mask representing what data to download
+ * @param [sinceDate] {Object|null} Date to retrieve data from (i.e. SMS received after this point)
+ * @param cb {callback} Callback
+ */
+riCloud.prototype.requestData = function(device, requestedData, sinceDate, cb) {
+    console.assert(this.sessionKey, 'Session key is required, please log in.')
+    var newArgs = argsWithFn(arguments, ['device', 'requestedData', 'sinceDate', 'cb']);
+    device = newArgs.device
+    console.assert(device, 'device is a required argument')
+    requestedData = newArgs.requestedData
+    sinceDate = newArgs.sinceDate
+    cb = newArgs.cb
+
+    if (!requestedData) {
+        // No mask has been set, so use everything
+        requestedData = 0
+        for (var key in this.data) {
+            if (this.data.hasOwnProperty(key)) {
+                requestedData |= this.data[key]
+            }
+        }
+    }
+
+    if (sinceDate == null) {
+        sinceDate = new Date(1900, 0, 1)
+    }
+
+    var since = moment(sinceDate).format('YYYY-MM-DD HH:mm:ss.SSS')
+
+    var data = {
+        key: this.sessionKey,
+        mask: requestedData,
+        since: since,
+        device: device
+    }
+    var options = generateOptions(this, 'download_data', data)
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body)
+            cb(0, data)
+        } else {
+            cb(context.error.GENERAL, response)
+        }
+    }
+
+    var context = this;
+    request(options, callback)
+}
+
+
+/**
+ * Download an individual file from the iCloud backup
+ * @param device {string}  Device ID to pull data for
+ * @param fileID {string} File ID representing the file we want to download
+ * @param [out] {string|null} Filename to write response to. If not provided we will return the object with in callback.
+ * @param cb {callback} Callback
+ */
+riCloud.prototype.downloadFile = function(device, fileID, out, cb) {
+    var newArgs = argsWithFn(arguments, ['device', 'fileID', 'out', 'cb']);
+    device = newArgs.device
+    console.assert(device, 'device is a required argument')
+    fileID = newArgs.fileID
+    console.assert(fileID, 'fileID is a required argument')
+    out = newArgs.out
+    cb = newArgs.cb
+
+    var data = {
+        key: this.sessionKey,
+        device: device,
+        file: fileID
+    }
+    var options = generateOptions(this, 'download_file', data)
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // If out is null, return the data as the result. Otherwise return null
+            var result = out ? null : body
+            cb(0, result)
+        } else {
+            cb(context.error.GENERAL, response)
+        }
+    }
+    var context = this;
+    if (out) {
+        request(options, callback).pipe(fs.createWriteStream(out))
+    } else {
+        request(options, callback)
+    }
+}
+
+
+/**
+ * Request a 2FA challenge to the supplied trusted device
+ * @param challengeDevice {string} The device that will display the challenge code
+ * @param cb {callback} Callback
+ */
+riCloud.prototype.requestTwoFAChallenge = function(challengeDevice, cb) {
+    var data = {
+        challenge: challengeDevice,
+        key: this.sessionKey
+    }
+    var options = generateOptions(this, 'challenge_2fa', data)
+
+    function callback(error, response, data) {
+        if (!error && response.statusCode == 200) {
+            // The challenge has been processed, we now need to wait
+            // for the user's submission
+            cb(0, null)
+        } else {
+            cb(context.error.GENERAL, response)
+        }
+    }
+
+    var context = this;
+    request(options, callback)
+}
+
+
+/**
+ * Submit a user supplied 2FA challenge code
+ * @param code {string} The code to supply
+ * @param cb {callback} Callback
+ */
+riCloud.prototype.submitTwoFAChallenge = function(code, cb) {
+    var data = {
+        code: code,
+        key: this.sessionKey
+    }
+    var options = generateOptions(this, 'submit_2fa', data)
+
+    function callback(error, response, data) {
+        if (!error && response.statusCode == 200) {
+            // Worked. Now user needs to log in again.
+            cb(0, null)
+        } else {
+            cb(context.error.GENERAL, response)
+        }
+    }
+
+    var context = this;
+    request(options, callback)
+}
+
+
+// Shared private functions
+
+function argsWithFn(args, names) {
+    /**
+     * Helper function to provide optional parameters
+     */
+    var newArgs = {};
+    var found = false;
+
+    // Loop through the arguments, starting from the end, and
+    // find the function.
+    for (var i = args.length - 1; i >= 0; --i) {
+
+        // If we find the function, put it in its expected place
+        // at the end of the arguments list.
+        if (!found && typeof args[i] == 'function') {
+            var fn = args[i];
+            newArgs[names[i]] = undefined;
+            newArgs[names[names.length - 1]] = fn;
+            found = true;
+        }
+
+        // Otherwise, just store the value in the new arguments.
+        else {
+            newArgs[names[i]] = args[i];
+        }
+    }
+    return newArgs;
+}
+
+
+function getEndpoint(endpointName) {
+    return HOST + ENDPOINTS[endpointName]
+}
+
+
+function generateOptions(context, endpoint, data) {
+    return {
+        url: getEndpoint(endpoint),
+        headers: {
+            'Accept': 'application/vnd.icloud-api.v' + API_VER
+          },
+        method: 'POST',
+        auth: context.auth,
+        form: data
+    }
+}
+
+
+module.exports = riCloud
